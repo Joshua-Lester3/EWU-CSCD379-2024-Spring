e559c9c0b69236641593ec1eec44c711b8cb5a78
Joshua Lester <jlester3@ewu.edu>
2024-04-24 10:01:52 -0700

    Building out home page + app bar

--- final/layouts/default.vue
+++ final/layouts/default.vue
@@ -1,13 +1,22 @@
 <template>
   <v-app>
     <v-app-bar>
-      <v-app-bar-title> Notes </v-app-bar-title>
-      <v-btn v-slot:append @click="$router.push('/document')">Doc</v-btn>
+      <v-app-bar-title class="cursor-pointer" @click="$router.push('/')">
+        Notes
+      </v-app-bar-title>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
+    <v-navigation-drawer location="right" v-model="drawer">
+      <v-list>
+        <v-list-item @click="$router.push('/')"> Home </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <slot />
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const drawer = ref(false);
+</script>
--- final/pages/index.vue
+++ final/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <v-container>
-    <v-row v-for="index in 10" :key="index">
-      <v-col cols="">
-        <v-card height="200" width="150">{{ index }}</v-card>
+  <v-container class="justify-center">
+    <v-btn @click="$router.push('/document')" icon="mdi-plus"></v-btn>
+  </v-container>
+  <v-container class="bg-surface-variant">
+    <v-row>
+      <v-col cols="4" v-for="n in 3" :key="n">
+        <v-card height="200" width="150">{{ n }}</v-card>
       </v-col>
     </v-row>
   </v-container>
