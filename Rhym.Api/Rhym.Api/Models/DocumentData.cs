01f280706372d8b94af0fe71744080f41233fcd0
Joshua Lester <jlester3@ewu.edu>
2024-05-09 09:22:54 -0700

    Small documentdata edit

--- Rhym.Api/Rhym.Api/Models/DocumentData.cs
+++ Rhym.Api/Rhym.Api/Models/DocumentData.cs
@@ -3,7 +3,6 @@
 public class DocumentData
 {
     public int DocumentDataId { get; set; }
-    public int DocumentId { get; set; }
     public string Title { get; set; } = null!;
 
     public string Content { get; set; } = null!;
